176c607f3d127e4c110d312bf353ea91946e4150
Tawanna Hakkinen <thol_16@hotmail.com>
2020-07-07 14:50:34 -0300

    configuração do swagger para documentar a api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDocument = require("../doc/swagger");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   return res.send("Hello world");
